0867b8033306063194efff2c20da01103c55cda1
Jolie Guerra <Jolie_guerra@yahoo.com>
2020-03-11 16:01:20 -0400

    add html file

---
+++ script.js
@@ -0,0 +1,22 @@
+var questionPosition = 0;
+
+var questions = [
+    {
+        title: "a",
+        choices: ["a", "b"],
+        answer: "a"
+    },
+    {
+        title: "c",
+        choices: ["c", "d"],
+        answer: "c"
+    },
+];
+
+function checkAnswer() {
+
+}
+
+function startQuiz() {
+    
+}
